d915b56bbf9ceb424c3fa48e91ef5c4da9131579
samiur2024 <samiur2019@gmail.com>
2024-11-14 20:25:55 +0600

    first commit

---
+++ Index.js
@@ -0,0 +1,13 @@
+var firstName="Samiur"
+var lastName="Rahaman"
+
+var fullName=firstName + " "+ lastName;
+
+var number1=10;
+var number2=5;
+number2=12;
+var total =number1 + number2;
+
+let names = "Samiur"
+
+console.log
